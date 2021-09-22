af2beb367bb2f786353cff5ab44f184bccdf1766
ao091 <89085226+ao091@users.noreply.github.com>
2021-09-22 19:16:45 +0200

    Create swap.py

---
+++ swap.py
@@ -0,0 +1,7 @@
+
+
+print("[+] Transferring ownership to @xzc9z9x0")
+print("[+] Fill in 2FA auth password")
+print("[+] Read input, transfer done. (Use -d or --delete to delete the telegram account to avoid any bans")
+print("-delete")
+print("Done, now press ^C to exit. Thanks")
